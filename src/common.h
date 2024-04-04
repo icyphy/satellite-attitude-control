5a118415a25d06f2b4e88cacc06627b6300b33c9
tanneberger <revol-xut@protonmail.com>
2024-04-04 18:19:15 +0200

    fixing some compilation problems

--- src/common.h
+++ src/common.h
@@ -1,28 +1,27 @@
 
+#ifndef COMMON_H
+#define COMMON_H
+
+
 typedef struct {
     uint32_t size;
     uint32_t descriptor;
     char* message;
 } TcpMessage;
 
-inline bool has_little_endian() {
-  unsigned int x = 1;
-  char *c = (char*) &x;
-  if ((int)*c == 1) {
-    return true;
-  }
-  return false;
-};
+inline bool has_little_endian();
 
 #define REVERSE_UINT32(n) ((uint32_t) ((((n) & 0xFF) << 24) | \
                                           (((n) & 0xFF00) << 8) | \
                                           (((n) & 0xFF0000) >> 8) | \
                                           (((n) & 0xFF000000) >> 24)))
 
-uint32_t convert_if_necessary(uint32_t value) {
-    if (has_little_endian()) {
-        return REVERSE_UINT32(value);
-    }
-    return value;
-};
+uint32_t convert_if_necessary(uint32_t value);
 
+typedef struct {
+    double angular_momentum;
+    double angle;
+    time_t time;
+} Telemetry;
+
+#endif //COMMON_H
