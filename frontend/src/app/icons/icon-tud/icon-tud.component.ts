0ffb74fc1d2f9085f29a26bd206aefaf178d3273
tanneberger <revol-xut@protonmail.com>
2024-04-14 23:36:53 +0200

    fixing frontend build problems

--- frontend/src/app/icons/icon-tud/icon-tud.component.ts
+++ frontend/src/app/icons/icon-tud/icon-tud.component.ts
@@ -6,7 +6,6 @@ import {CommonModule} from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './icon-tud.component.svg',
-  styleUrls: ['../../../style.scss'],
 })
 export class IconTudComponent {
 
